6cadee0e79a72b369d0eac722ec92f27a55b1d63
Rafał Pawłaszek <rafal.pawlaszek@capgemini.com>
2021-07-15 13:44:41 +0200

    Removed an unnecessary property

--- src/DevHours.CloudNative.Api/Data/Dtos/BookingDto.cs
+++ src/DevHours.CloudNative.Api/Data/Dtos/BookingDto.cs
@@ -7,7 +7,6 @@ namespace DevHours.CloudNative.Api.Data.Dtos
         public int Id { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
-        public bool IsActive { get; set; }
         public int RoomId { get; set; }
         public virtual RoomDto Room { get; set; }
     }
--- src/DevHours.CloudNative.Core/Domain/Booking.cs
+++ src/DevHours.CloudNative.Core/Domain/Booking.cs
@@ -7,7 +7,6 @@ namespace DevHours.CloudNative.Domain
         public int Id { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
-        public bool IsActive { get; set; }
         public int RoomId { get; set; }
         public virtual Room Room { get; set; }
     }
